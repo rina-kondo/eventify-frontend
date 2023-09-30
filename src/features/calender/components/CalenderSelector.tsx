aa06ce6f571d996c2d69bca30083a6cf7996baee
rina-kondo <666.k.rina@gmail.com>
2023-10-01 06:11:45 +0900

    style: :art: コンポネント名誤記修正

--- src/features/calender/components/CalenderSelector.tsx
+++ src/features/calender/components/CalenderSelector.tsx
@@ -5,9 +5,9 @@ import { useCalenderContext } from '@/store/calender-theme';
 import { useTargetDateContext } from '@/store/target-date';
 import { getDay, getWeek, getMonth } from '@/features/calender/utils/dayjs';
 
-const Month = React.lazy(() => import('./organisms/MonthCalender'));
-const Week = React.lazy(() => import('./organisms/WeekCalender'));
-const Day = React.lazy(() => import('./organisms/DayCalender'));
+const MonthCalender = React.lazy(() => import('./organisms/MonthCalender'));
+const WeekCalender = React.lazy(() => import('./organisms/WeekCalender'));
+const DayCalender = React.lazy(() => import('./organisms/DayCalender'));
 
 export default function CalenderSelector() {
   const { calenderTheme } = useCalenderContext();
@@ -18,9 +18,9 @@ export default function CalenderSelector() {
   const today = getDay();
 
   const componentMap: { [key: string]: JSX.Element } = {
-    month: <Month targetMonth={targetMonth} today={today} />,
-    week: <Week targetWeek={targetWeek} today={today} />,
-    day: <Day targetDate={targetDate} today={today} />,
+    month: <MonthCalender targetMonth={targetMonth} today={today} />,
+    week: <WeekCalender targetWeek={targetWeek} today={today} />,
+    day: <DayCalender targetDate={targetDate} today={today} />,
   };
 
   const CalenderComponent = componentMap[calenderTheme];
